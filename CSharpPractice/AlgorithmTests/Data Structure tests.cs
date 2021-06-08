b017f2aae7390c18cc5106a09dd91e3c8f9804e9
X <Nhlapho7@gmail.com>
2021-06-08 04:48:28 +0200

    Implementation of Queue Data Structure

---
+++ CSharpPractice/AlgorithmTests/Data Structure tests.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using NUnit.Framework;
+using QDS = CSharpPractice.Data_Structures;
+
+namespace AlgorithmTests
+{
+    [TestFixture]
+    public class Queue_Data_Structure_tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Queue_Data_Structure()
+        {
+            QDS.Queue q = new QDS.Queue();
+            q.enqueue(10);
+            q.enqueue(20);
+            q.dequeue();
+            q.dequeue();
+            q.enqueue(30);
+            q.enqueue(40);
+            q.enqueue(50);
+            q.dequeue();
+            Assert.AreEqual(40, q.front.key);
+            Assert.AreEqual(50, q.rear.key);
+        }
+    }
+}
---
+++ CSharpPractice/CSharpPractice/Data Structures/Queue Data Structure.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpPractice.Data_Structures
+{
+    public class QNode
+    {
+        public int key;
+        public QNode next;
+
+        public QNode(int key)
+        {
+            this.key = key;
+            this.next = null;
+        }
+    }
+    public class Queue
+    {
+        public QNode front, rear;
+        public Queue()
+        {
+            this.front = this.rear = null;
+        }
+        public void enqueue(int key)
+        {
+            QNode temp = new QNode(key);
+            if(this.rear == null)
+            {
+                this.front = this.rear = temp;
+                return;
+            }
+
+            this.rear.next = temp;
+            this.rear = temp;
+        }
+
+        public void dequeue()
+        {
+            if (this.front == null)
+                return;
+
+            QNode temp = this.front;
+            this.front = this.front.next;
+
+            if (this.front == null)
+                this.rear = null;
+        }
+    }
+}
